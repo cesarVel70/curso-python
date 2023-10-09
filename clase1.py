48f8afd616003252e6a5e26932fbf2f75bda740b
cesarVel70 <contadorcesarveliz@gmail.com>
2023-10-09 20:49:18 -0300

    subir hasta el punto 5 resuelto inclusive

---
+++ clase1.py
@@ -0,0 +1,39 @@
+#1. Codifique un programa que solicite que el usuario ingrese un número entero y presente el número ingrado en pantalla
+
+# num = int(input("Ingrese un número: "))
+# print("El número ingresado es: ",num)
+#2. Codifique un programa que solicite al usuario que ingrese dos números enteros positivos a continuación calcule las siguientes operaciones: suma, resta, multiplicación, división, división entera y módulo y los presente en pantalla
+
+# num1 = int(input("Ingrese un número entero: "))
+# num2 = int(input("Ingrese otro número entero: "))
+
+# print("La suma de los números ingresados es: ",num1+num2)
+# print("La resta de los números ingresados es: ",num1-num2)
+# print("La multiplicación de los números ingresados es: ",num1*num2)
+# print("La división de los números ingresados es: ",num1/num2)
+# print("La división entera de los números ingresados es: ",int((num1/num2)))
+# print("El módulo de los números ingresados es: ",num1%num2)
+#3. Codifique un programa que le solicite el día de la  semana que es (lunes, martes, miercoles, jueves, viernes, sábado o domingo) y presente en pantalla el nombre del día ingresado
+
+# dia = input("Ingrese el día que es: ")
+# print("El día ingresado es: ",dia)
+#4. Codifique un programa que solicite su nombre y apellido y presente en pantalla su apellido y nombre en ese orden
+
+# nombre = input("Ingrese su nombre: ")
+# apellido = input("Ingrese su apellido: ")
+# print("Su apellido es ",apellido," y su nombre es ",nombre, )
+#5. Codifique un programa que solicite el ingreso de un número entero distinto de cero y presente en pantalla si es positivo o negativo
+
+# num = int(input("Ingrese un número entero distinto de cero: "))
+
+# if num > 0:
+#     print("El número ingresado es positivo")
+# else:
+#     print("El número ingresado es negativo")
+#6. Codifique un programa que solicite el ingreso de un número entero distinto de cero y presente en pantalla si es positivo, negatio o cero
+
+
+    
+
+
+
